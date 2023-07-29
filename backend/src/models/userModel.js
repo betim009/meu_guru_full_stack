18dffaa620b3a9d96f5150f3073b06573989976a
Alberto <betim009@gmail.com>
2023-07-28 22:28:47 -0300

    feat: CRUD model

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -7,6 +7,42 @@ const getAll = async () => {
     return result;
 };
 
+const getById = async (id) => {
+    const [[user]] = await connection.execute(
+        'SELECT * FROM usuarios WHERE id = ?',
+        [id]
+    );
+    return user;
+};
+
+const create = async (nome, email, password, idade, sexo, cidade) => {
+    const [{ insertId }] = await connection.execute(
+      'INSERT INTO usuarios (nome, email, password, idade, sexo, cidade) VALUES (?, ?, ?, ?, ?, ?)',
+      [nome, email, password, idade, sexo, cidade]
+    );
+    return insertId;
+  };
+
+const update = async (id, nome, email, password) => {
+    const result = await connection.execute(
+        'UPDATE usuarios SET nome = ?, email = ?, password = ? WHERE id = ?',
+        [nome, email, password, id]
+    );
+    return result;
+};
+
+const deleteById = async (id) => {
+    const result = await connection.execute(
+        'DELETE FROM usuarios WHERE id = ?',
+        [id]
+    );
+    return result;
+};
+
 module.exports = {
     getAll,
-}
+    getById,
+    create,
+    update,
+    deleteById,
+};
