311c039b639dd895fac9af0e71c2cbd0a0ca50c0
Alberto <betim009@gmail.com>
2023-07-28 21:50:37 -0300

    feat: Iniciando o backend

---
+++ backend/seed.sql
@@ -0,0 +1,22 @@
+USE meu_guru;
+
+INSERT INTO usuarios (nome, email, password, idade, sexo, cidade)
+VALUES
+  ('João Silva', 'joao.silva@meuguru', 'guru123', 28, 'Masculino', 'Serra'),
+  ('Maria Souza', 'maria.souza@meuguru', 'guru456', 35, 'Feminino', 'Serra'),
+  ('Pedro Oliveira', 'pedro.oliveira@meuguru', 'guru789', 22, 'Masculino', 'Belo Horizonte'),
+  ('Ana Santos', 'ana.santos@meuguru', 'guru987', 30, 'Feminino', 'Salvador'),
+  ('José Pereira', 'jose.pereira@meuguru', 'guru321', 26, 'Masculino', 'Brasília'),
+  ('Carla Costa', 'carla.costa@meuguru', 'guru654', 29, 'Feminino', 'Serra'),
+  ('Mário Fernandes', 'mario.fernandes@meuguru', 'guru987', 32, 'Masculino', 'Porto Alegre'),
+  ('Fernanda Oliveira', 'fernanda.oliveira@meuguru', 'guru654', 31, 'Feminino', 'Serra'),
+  ('Ricardo Almeida', 'ricardo.almeida@meuguru', 'guru321', 27, 'Masculino', 'Fortaleza'),
+  ('Juliana Silva', 'juliana.silva@meuguru', 'guru789', 24, 'Feminino', 'Manaus'),
+  ('Gabriel Santos', 'gabriel.santos@meuguru', 'guru123', 33, 'Masculino', 'Belém'),
+  ('Larissa Costa', 'larissa.costa@meuguru', 'guru456', 25, 'Feminino', 'Vitória'),
+  ('Daniel Oliveira', 'daniel.oliveira@meuguru', 'guru987', 28, 'Masculino', 'Porto Alegre'),
+  ('Mariana Fernandes', 'mariana.fernandes@meuguru', 'guru654', 29, 'Feminino', 'Salvador'),
+  ('Gustavo Pereira', 'gustavo.pereira@meuguru', 'guru321', 32, 'Masculino', 'Serra'),
+  ('Carolina Costa', 'carolina.costa@meuguru', 'guru789', 27, 'Feminino', 'Recife'),
+  ('Fábio Oliveira', 'fabio.oliveira@meuguru', 'guru654', 34, 'Masculino', 'Curitiba'),
+  ('Aline Almeida', 'aline.almeida@meuguru', 'guru321', 26, 'Feminino', 'Fortaleza');
