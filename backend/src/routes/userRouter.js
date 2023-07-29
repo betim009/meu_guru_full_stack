d8c70e79a3853f879eb660a0a79ddbc2c840dff0
Alberto <betim009@gmail.com>
2023-07-28 22:29:49 -0300

    feat: Rotas

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -4,5 +4,9 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/', userController.getAllController);
+router.get('/:id', userController.getById);
+router.post('/', userController.createUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
