3d0c9f81fd7bb4168a7c9908a29d693037212338
Alberto <betim009@gmail.com>
2023-07-28 22:21:50 -0300

    fix: Adicionando cors e rotas

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const userRoute = require('./src/routers/userRouter')
 
 const app = express();
 
-
+app.use(cors()); 
 app.use(express.json());
-
+app.use('/users', userRoute)
 
 module.exports = app;
