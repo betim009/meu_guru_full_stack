77ac56dca3eb6ce20fd3ecc600a9300266f5b703
Alberto <betim009@gmail.com>
2023-07-28 22:27:31 -0300

    feat: CRUD service

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,10 +1,34 @@
-const { getAll } = require('../models/userModel');
+const { getAll, getById, create, update, deleteById } = require('../models/userModel');
 
 const getAllServices = async () => {
     const users = await getAll();
     return users;
 };
 
+const getByIdServices = async (id) => {
+    const user = await getById(id);
+    return user;
+};
+
+const createServices = async (nome, email, password, idade, sexo, cidade) => {
+    const insertId = await create(nome, email, password, idade, sexo, cidade);
+    return insertId;
+};
+
+const updateServices = async (id, nome, email, password) => {
+    const user = await update(id, nome, email, password);
+    return user;
+};
+
+const deleteServices = async (id) => {
+    const user = await deleteById(id);
+    return user;
+};
+
 module.exports = {
     getAllServices,
-}
+    getByIdServices,
+    createServices,
+    updateServices,
+    deleteServices,
+};
