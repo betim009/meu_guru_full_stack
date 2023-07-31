86519d53f9a875816096cdd3ef59347bcbaea56a
Alberto <betim009@gmail.com>
2023-07-31 14:49:04 -0300

    test: Teste do frontend

--- frontend-typescript/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import App from './App';
-
-it('should show "Vite + React" text', () => {
-  render(<App />);
-  expect(screen.getByText(/vite \+ react/i)).toBeInTheDocument();
-})
